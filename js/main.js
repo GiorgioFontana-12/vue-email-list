16f2d4ca62ac0f851fa8a291b61ba1bfc647b318
Giorgio Fontana <giorgiofontanaj@gmail.com>
2023-05-17 18:50:10 +0200

    first attempt axios

--- js/main.js
+++ js/main.js
@@ -3,15 +3,25 @@ const { createApp } = Vue
 createApp({
     data() {
         return{
+            emails:[],
+            randomEmailsApi: 'https://flynn.boolean.careers/exercises/api/random/mail'
         }
     },
 
     methods: {
-
+        
     },
 
 
     mounted() {
-
+        for (let index = 0; index < 9; index++) {
+            axios.get( this.randomEmailsApi ).then((emails) => {
+                let newemail = emails.data.response;
+                console.log(newemail)
+                this.emails.push(newemail)
+            });
+            
+        }
+       
     },
 }).mount('#app')
